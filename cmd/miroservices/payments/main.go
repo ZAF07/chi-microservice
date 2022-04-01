82fe2e29331fe091ad5fe0b6c212431a770fa05a
ZAF07 <zaffere07@gmail.com>
2022-04-01 14:08:20 +0800

    First commit, skeleton

---
+++ cmd/miroservices/payments/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main () {
+	log.Println("STARTING PAYMENTS MICROSERVICE")
+
+	defer log.Println("Closing payments microservice")
+
+	ctx := cmd.Context()
+
+	paymentsinterface := createPaymentsmicroservice()
+	if err := paymentsinterface.Run(ctx); err != nil {
+		panic(err)
+	}
+}
+
+func createPaymentsmicroservice() amqp.paymentsinterface {
+	cmd.waitForService(os.Getenv("SHOP_RABBITMQ_ADDR"))
+	
+	paymentsService := payments_app.NewPaymentsService(payments_infra_orders.NewHTTPClient(os.Getenv("SHOP_ORDERS_ADDR")))
+
+	 paymentsInterface, err := amqp.NewPaymentsInterface(
+		 fmt.Sprintf("amqp://%s/", os.Getenv("SHOP_RABBITMQ_ADDR")),
+		 os.Getenv("SHOP_RABBITMQ_ORDERS_TO_PAY_QUEUE"),
+		 paymentsService, 
+	 )
+
+	 if err != nil {
+		 panic(err)
+	 }
+	 return paymentsInterface
+}
---
+++ pkg/common/cmd/wait.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"log"
+	"net"
+	"time"
+)
+
+// Takes in RabbitMQ host string and connects to RabbitMQ, returns RabbitMQ Connection instance
+func WaitForService(host string) {
+	log.Println("Waiting for host %s", host)
+
+	for {
+		log.Println("testing cinnection to %s", host)
+		conn, err := net.Dial("tcp", host)
+		if err == nil {
+			_ = conn.Close()
+			log.Printf("%s is up!", host)
+			return
+		}
+		time.Sleep(time.Millisecond *500)
+	}
+}
